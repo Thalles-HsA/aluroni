651085517513828333004d8ce499242b36007c4d
Thalles-HsA <thsa.henrique@gmail.com>
2023-02-08 22:00:43 -0300

    Projeto React com TypeScript - Arquivos Estáticos

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,17 @@
+import styles from './Cardapio.module.scss';
+import {ReactComponent as Logo} from '../../assets/logo.svg';
+
+
+function Cardapio () {
+
+  return (
+    <main>
+        <nav className={styles.menu}>
+            <Logo />
+        </nav>
+    </main>
+  
+    );
+}
+
+export default Cardapio;
